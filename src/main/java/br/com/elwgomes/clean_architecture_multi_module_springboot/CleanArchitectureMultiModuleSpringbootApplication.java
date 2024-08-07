aeb3b37ef44b41602fd94835249503f24fa70e82
Leonardo <elwgomes@icloud.com>
2024-08-07 14:55:09 -0300

    new project

---
+++ src/main/java/br/com/elwgomes/clean_architecture_multi_module_springboot/CleanArchitectureMultiModuleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.elwgomes.clean_architecture_multi_module_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureMultiModuleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureMultiModuleSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elwgomes/clean_architecture_multi_module_springboot/CleanArchitectureMultiModuleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elwgomes.clean_architecture_multi_module_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureMultiModuleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
