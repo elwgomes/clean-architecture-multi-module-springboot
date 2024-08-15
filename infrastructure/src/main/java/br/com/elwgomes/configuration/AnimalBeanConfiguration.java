18bc6e88273724595a0d442d159ff7f8f006de95
Leonardo <elwgomes@icloud.com>
2024-08-15 12:03:40 -0300

    add: comments

--- infrastructure/src/main/java/br/com/elwgomes/configuration/AnimalBeanConfiguration.java
+++ infrastructure/src/main/java/br/com/elwgomes/configuration/AnimalBeanConfiguration.java
@@ -14,6 +14,7 @@ import br.com.elwgomes.usecase.getAllAnimals.GetAllAnimalsHandler;
 @Configuration
 public class AnimalBeanConfiguration {
 
+  // animal spi
   @Bean
   AnimalDatabasePort animalDatabasePort(AnimalRepository animalRepository) {
     return new AnimalDatabaseAdapter(animalRepository);
